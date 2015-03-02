5918c5c576d03446ec520c7d3e7ac7f384be997f
Maik Srba <maik.srba@gwdg.de>
2015-03-02 20:35:49 +0100

    https://tickets.puppetlabs.com/browse/PUP-1386

--- scripts/provision_puppetmaster.sh
+++ scripts/provision_puppetmaster.sh
@@ -66,6 +66,9 @@ ln -s /vagrant/scripts/files/site.pp $PUPPET_ENVIRONMENT/manifests/
 # Setup hiera.yaml
 cat /vagrant/scripts/files/hiera/hiera.yaml > /etc/puppet/hiera.yaml
 
+# Fix Error 400 on SERVER: invalid byte sequence in US-ASCII at ... https://tickets.puppetlabs.com/browse/PUP-1386
+echo "LANG=en_US.UTF-8" >> /etc/default/puppetmaster
+
 # Restart puppetmaster
 service puppetmaster restart
 
